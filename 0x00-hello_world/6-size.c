98ad16b2be0fb6e2414790b9d2a920a03a60b5e7
Timalex1 <timilehinvincent@gmail.com>
2021-06-17 11:04:06 -0700

    Adding file to print size of variou types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ *main - "Size of various types"
+ *Return: 0
+ */
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+	long longType;
+	long long longLongType;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long: %zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
+}

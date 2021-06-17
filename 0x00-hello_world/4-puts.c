e6aedefeb05f42bbc0b6ae9fb603f324d0197ea5
Timalex1 <timilehinvincent@gmail.com>
2021-06-17 09:43:02 -0700

    This file contains C commands

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  *main fnx prints out ""Programming is like building a multilingual puzzle""
  *Returns void/0/nothing
  */
-int main(void){
+int main(void)
+{
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }

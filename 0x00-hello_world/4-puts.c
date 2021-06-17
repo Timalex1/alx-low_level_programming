04076b816397375833ae99a0786eaf5bb8a8ad11
Timalex1 <timilehinvincent@gmail.com>
2021-06-17 10:25:33 -0700

    Fixing indentation problem

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 0;136;0c *main fnx prints out ""Programming is like building a multilingual puzzle""
  *Returns void/0/nothing
  */
-int main(void)
-{ 
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+int main(void
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

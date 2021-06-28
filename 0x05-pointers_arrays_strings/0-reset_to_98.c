03d697e970c94f6585079a22c6623b02f825dfe2
Timalex1 <timilehinvincent@gmail.com>
2021-06-28 08:36:22 -0700

    Fixing file directory

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - sets the integer to 98
+ * @n: a pointer to the integer we are to set to 98
+ * Return: (0)
+ */
+
+int reset_to_98(int *n)
+{
+	*n = 98;
+}

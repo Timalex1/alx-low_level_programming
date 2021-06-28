1fc922ae6142baa6fa1c3a34db6d9855a35cd710
Timalex1 <timilehinvincent@gmail.com>
2021-06-28 08:49:02 -0700

    2ND TASK

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#incllude "holberton.h"
+
+/**
+ * swap_int - swap values of 2 integers
+ * @a:
+ * @b:
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}

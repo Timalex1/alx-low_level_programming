8dc0b11fb68f9e4ca310a4514cd6c0e2c7b028be
Timalex1 <timilehinvincent@gmail.com>
2021-07-01 07:17:21 -0700

    adding files

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,6 @@
  * @n: length of src.
  * Return: char
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
@@ -17,11 +16,7 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 	}
-
 	for (; i < n; i++)
 		dest[i] = '\0';
-
-	
 	return (dest);
-
 }

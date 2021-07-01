97b4f858f48c5029151c16ce694346dfb65047de
Timalex1 <timilehinvincent@gmail.com>
2021-07-01 07:09:51 -0700

    commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -41,14 +41,14 @@ char *_strncat(char *dest, char *src, int n)
 	int length;
 
 	if (n > _strlen(src) +_strlen(dest))
-	  {
+	{
 		length = _strlen(dest) + _strlen(src);
-	  }
+	}
 
 	else
-	  {
+	{
 		length = _strlen(dest) + n;
-	  }
+        }
 
 	while (*src && n > 0)
 	{
@@ -59,7 +59,7 @@ char *_strncat(char *dest, char *src, int n)
 	}
 
 	if (n > 0)
-		*cat += '\0';
+	*cat += '\0';
 	cat -= (length);
 	*dest = *cat;
 

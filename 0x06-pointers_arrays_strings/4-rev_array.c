34c0d022183d1962230a332cb03a63057a2c703a
Timalex1 <timilehinvincent@gmail.com>
2021-07-01 07:33:24 -0700

    Adding files

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,32 +14,6 @@ void reverse_array(int *a, int n)
 	int l = n - 1, i = 0;
 	int tmp;
 
-	while (i < ((n + 1) / 1))
-	  {
-		tmp = *(a + l);
-		*(a + l) = *(a + i);
-		*(a + i) = tmp;
-		i++;
-		l--;
-	  }
-
-}
-#include "holberton.h"
-
-/**
- * reverse_array - reverses the content of an array of integers.
- *
- * @a: array to reverse
- * @n: number of elements of the array
- *
- * Return: void
- */
-
-void reverse_array(int *a, int n)
-{
-	int l = n - 1, i = 0;
-	int tmp;
-
 	while (i <  ((n + 1) / 2))
 	{
 		tmp = *(a + l);

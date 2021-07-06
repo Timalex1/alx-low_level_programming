3d3114bb4c8e5a04eb544c0fd8d252a1647dbcd5
Timalex1 <timilehinvincent@gmail.com>
2021-07-06 03:51:29 -0700

    Correcting error in 100

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
-**
- *set_string - sets the value of a pointer to a char.
+/**
+ * set_string - sets the value of a pointer to a char.
  * @s: pointer to pointer
  * @to: pointer char
  *

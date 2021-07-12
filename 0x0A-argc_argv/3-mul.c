a66d147731e8ac58449c8ed60d8a975a1d94a693
Timalex1 <timilehinvincent@gmail.com>
2021-07-12 14:12:34 -0700

    Adding files

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ ** main - program that multiplies two numbers
+ * @argc: arguments count
+ * @argv: array of arguments
+ * Return: 1 when error
+ * exit: EXIT_SUCCESS
+ */
+
+int main(int argc, char *argv[])
+{
+	if(argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		exit(EXIT_SUCCESS);
+	} else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}

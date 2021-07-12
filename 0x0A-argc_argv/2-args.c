50260fc952df93d7349e97c859dbeb83bac24e83
Timalex1 <timilehinvincent@gmail.com>
2021-07-12 14:03:20 -0700

    Adding files

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+* main - program that prints all arguments it receives
+ * @argc: arguments count
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+
+	exit(EXIT_SUCCESS);
+}

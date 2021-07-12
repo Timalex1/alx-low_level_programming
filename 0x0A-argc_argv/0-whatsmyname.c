2731a10554747b22794babfe7042449901571db4
Timalex1 <timilehinvincent@gmail.com>
2021-07-12 13:53:34 -0700

    Adding second file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: arguments count
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc __atrribute__((unused)), char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	exit(EXIT_SUCCESS);
+}

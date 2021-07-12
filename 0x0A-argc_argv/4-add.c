6fd86ac88b6eaadec294b20428e5627177bd97f2
Timalex1 <timilehinvincent@gmail.com>
2021-07-12 14:29:43 -0700

    Adding files

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,26 +2,23 @@
 #include <stdlib.h>
 
 /**
- *
  * isInteger - checks if s is an integer
  * @s: string to check
  * Return: 0 or 1
  */
 
-
 int isInteger(const char *s)
 {
+int i = 0;
+while (s[i] != '\0')
+{
+	if (s[i] < '0' || s[i] > '9')
+		return (1);
+	i++;
+}
+ 
+return (0);
 
-  int i = 0;
-  while (s[i] != '\0')
-    {
-      if (s[i] < '0' || s[i] > '9')
-	return {1);
-
-      i++;
-    }
-
-  return (0);
 }
 
 
@@ -34,28 +31,25 @@ int isInteger(const char *s)
  */
 
 
-
 int main(int argc, char *argv[])
 {
 
-  int sum = 0;
+	 int sum = 0;
 
-  if (argc < 2)
-    {
-      printf("0\n");
-      return (0);
-    }
-  while (--argc)
-    {
-      if (isInteger(argv[argc]))
+	if (argc < 2)
 	{
-	  printf("Error\n");
-	  return (1);
+		printf("0\n");
+		return (0);
 	}
-
-      sum += atoi(argv[argc]);
-    }
-
-  printf("%d\n", sum);
-  exit(EXIT_SUCCESS);
+	while (--argc)
+	{
+		if (isInteger(argv[argc]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	exit(EXIT_SUCCESS);
 }

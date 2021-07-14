ddb756ce9880644be3141c0d0eb9044d07a98c26
Timalex1 <timilehinvincent@gmail.com>
2021-07-14 12:02:19 -0700

    Third commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+
+/**
+ * _strlen - return length of string
+ *
+ * @s: string to count
+ *
+ * Return: the size
+ */
+
+
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (*s != 0)
+	{
+
+		counter++;
+		s++;
+
+	}
+	return (counter);
+}
+
+/**
+ * *str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to the result
+ */
+
+
+
+char *str_concat(char *s1, char *s2)
+{
+	int l1 = s1 == NULL ? 0 : _strlen(s1);
+	int l2 = s2 == NULL ? 0 : _strlen(s2);
+	char *p = (char *)malloc(l1+l2+1);
+
+	if (p == NULL)
+		return (NULL);
+
+	if (s1 != NULL)
+	{
+	while (*s1)
+	{
+		*p = *s1;
+		s1++;
+		p++;
+	}
+	}
+	if (s2 != NULL)
+	{
+	while (*s2)
+	{
+		*p = *s2;
+		s2++;
+		p++;
+	}
+	}
+	p -= l1 + l2;
+	return (p);
+
+}
+
+	  

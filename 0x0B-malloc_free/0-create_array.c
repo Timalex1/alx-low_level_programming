c04ec570ab8079d3490586cab7d14cddb3746698
Timalex1 <timilehinvincent@gmail.com>
2021-07-14 11:25:17 -0700

    Adding files second

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and
+ * initializes it with a specific char.
+ * @size: size of array
+ * @c: character to initialize it with
+ * Return: Null if size = 0 or if it fils and a pointer to the arrayif succed
+ */
+
+
+char create_array(unsigned int size, char c)
+{
+
+	char *p;
+
+	if (!size)
+		return (NULL);
+	
+
+	P = (char *)malloc(size);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (size)
+	{
+		p[size -1] = c;
+		size--;
+	}
+
+        return (p);
+}
+  

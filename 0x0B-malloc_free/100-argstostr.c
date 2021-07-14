5a3510f7cefea8e604f87d1f70b03f991d91d965
Timalex1 <timilehinvincent@gmail.com>
2021-07-14 13:40:17 -0700

    Fixing error

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -30,10 +30,10 @@ int _strlen(char *s)
  */
 
 
-char *argstostr(int ac, char **Av)
+char *argstostr(int ac, char **av)
 {
 
-	int i = 0; sizecounter = 0, j = 0, counter = 0;
+	int i = 0, sizecounter = 0, j = 0, counter = 0;
 	char *p;
 
 	if (ac == 0 || ac == NULL)
